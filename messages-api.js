a0632264352be6c0eaf52a4c15f5251ed27139de
Mellieejj <Melanieverheul1992@gmail.com>
2020-02-29 12:50:39 +0100

    response print, bad request add

--- messages-api.js
+++ messages-api.js
@@ -1,11 +1,24 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 const port = 3000;
 
-app.get("/messages", (request, response, next) => {
-  response.send("Hello");
+app.use(bodyParser.json());
+
+app.post("/", (request, response, next) => {
+  console.log(request.body.message);
+  const responseText =
+    request.body.message || response.status(400).json("Bad Request");
+  response.json({
+    text: responseText
+  });
 });
+
+// app.get("/messages", (request, response, next) => {
+//   response.send("Hello");
+// });
+
 app.listen(port, () => {
   console.log(`app listen on ${port}`);
 });
