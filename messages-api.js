b61213dcc152d6d21db7fff20b3a0567412baf49
Mellieejj <Melanieverheul1992@gmail.com>
2020-02-29 13:19:10 +0100

    add counting requests

--- messages-api.js
+++ messages-api.js
@@ -4,20 +4,28 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
+let counting = 0;
+
+const requestCounting = function(request, response, next) {
+  if (counting >= 5) {
+    return response.status(429).next();
+  } else {
+    next();
+  }
+};
+
+app.use(requestCounting);
 app.use(bodyParser.json());
 
 app.post("/", (request, response, next) => {
-  console.log(request.body.message);
+  console.log(request.body);
+  counting++;
   const responseText =
-    request.body.message || response.status(400).json("Bad Request");
+    request.body.text || response.status(400).json("Bad Request");
   response.json({
     text: responseText
   });
 });
-
-// app.get("/messages", (request, response, next) => {
-//   response.send("Hello");
-// });
 
 app.listen(port, () => {
   console.log(`app listen on ${port}`);
